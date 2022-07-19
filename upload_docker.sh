1087cdef762db211b601ac699221621e0433b8f5
Essameldeen Youssef <eeid@newstore.com>
2022-07-19 04:48:27 +0200

    final

--- upload_docker.sh
+++ upload_docker.sh
@@ -10,7 +10,6 @@ dockerpath=essameid614/udacity_nanodegree_capstone
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-docker tag udacity_nanodegree_capstone $dockerpath
 docker images
 # Step 3:
 # Push image to a docker repository
