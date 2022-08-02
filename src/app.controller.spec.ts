41c1c64ec391fa0fedbe82e2ce6f9236e9c145e8
Essameldeen Youssef <eeid@newstore.com>
2022-08-02 05:40:07 +0200

    typo fix

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World from new deployment!"', () => {
-      expect(appController.getHello()).toBe('Hello World from new deployment!');
+      expect(appController.getHello()).toBe('Hello world from new deployment!');
     });
   });
 });
